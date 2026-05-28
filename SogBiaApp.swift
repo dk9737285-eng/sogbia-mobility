905ea9df705ceb8fa6ea9e1ab1983209e721fb95
dk9737285-eng <dk9737285@gmail.com>
2026-05-28 20:11:57 +0100

    Add files via upload

---
+++ SogBiaApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct SogBiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark) // Standardizes theme appearance to match SogBia's premium design
+        }
+    }
+}
